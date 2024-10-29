cf2d83c6dde56b688f2d8285e195aff7493dcd0d
Renanvaladares <valadaresren@gmail.com>
2024-10-28 22:42:27 -0300

    Seu primeiro commit

--- script.js
+++ script.js
@@ -492,8 +492,8 @@ const App = () => {
             React.createElement(Pin, null),
             React.createElement(Menu, null),
             React.createElement(Background, null),
-            React.createElement("div", { id: "sign-in-button-wrapper" },
-                React.createElement(UserStatusButton, { icon: "fa-solid fa-arrow-right", id: "sign-in-button", userStatus: UserStatus.LoggingIn })),
+          //  React.createElement("div", { id: "sign-in-button-wrapper" },
+          //      React.createElement(UserStatusButton, { icon: "fa-solid fa-arrow-right", id: "sign-in-button", userStatus: UserStatus.LoggingIn })),
             React.createElement(Loading, null))));
 };
 ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
